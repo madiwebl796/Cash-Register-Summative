01e700846313560a42fe9c70e44b093f8f57d591
madiwebl796 <madiwebl796@ed.amdsb.ca>
2016-10-13 12:48:12 -0400

    Add project files.

---
+++ Cash Register Summative/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Cash_Register_Summative
+{
+    partial class cashRegisterSummative
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lipkitTitle = new System.Windows.Forms.Label();
+            this.candyKInput = new System.Windows.Forms.TextBox();
+            this.candyKLabel = new System.Windows.Forms.Label();
+            this.dolceKLabel = new System.Windows.Forms.Label();
+            this.lovebiteLabel = new System.Windows.Forms.Label();
+            this.freedomLabel = new System.Windows.Forms.Label();
+            this.dolceKInput = new System.Windows.Forms.TextBox();
+            this.lovebiteInput = new System.Windows.Forms.TextBox();
+            this.freedomInput = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.CheckoutButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lipkitTitle
+            // 
+            this.lipkitTitle.AutoSize = true;
+            this.lipkitTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lipkitTitle.Location = new System.Drawing.Point(1, 9);
+            this.lipkitTitle.Name = "lipkitTitle";
+            this.lipkitTitle.Size = new System.Drawing.Size(193, 29);
+            this.lipkitTitle.TabIndex = 0;
+            this.lipkitTitle.Text = "Lipkit knockoffs";
+            // 
+            // candyKInput
+            // 
+            this.candyKInput.Location = new System.Drawing.Point(171, 62);
+            this.candyKInput.Name = "candyKInput";
+            this.candyKInput.Size = new System.Drawing.Size(100, 20);
+            this.candyKInput.TabIndex = 1;
+            // 
+            // candyKLabel
+            // 
+            this.candyKLabel.AutoSize = true;
+            this.candyKLabel.Location = new System.Drawing.Point(13, 65);
+            this.candyKLabel.Name = "candyKLabel";
+            this.candyKLabel.Size = new System.Drawing.Size(47, 13);
+            this.candyKLabel.TabIndex = 2;
+            this.candyKLabel.Text = "Candy K";
+            // 
+            // dolceKLabel
+            // 
+            this.dolceKLabel.AutoSize = true;
+            this.dolceKLabel.Location = new System.Drawing.Point(13, 124);
+            this.dolceKLabel.Name = "dolceKLabel";
+            this.dolceKLabel.Size = new System.Drawing.Size(45, 13);
+            this.dolceKLabel.TabIndex = 3;
+            this.dolceKLabel.Text = "Dolce K";
+            // 
+            // lovebiteLabel
+            // 
+            this.lovebiteLabel.AutoSize = true;
+            this.lovebiteLabel.Location = new System.Drawing.Point(6, 181);
+            this.lovebiteLabel.Name = "lovebiteLabel";
+            this.lovebiteLabel.Size = new System.Drawing.Size(52, 13);
+            this.lovebiteLabel.TabIndex = 4;
+            this.lovebiteLabel.Text = "Love Bite";
+            // 
+            // freedomLabel
+            // 
+            this.freedomLabel.AutoSize = true;
+            this.freedomLabel.Location = new System.Drawing.Point(6, 237);
+            this.freedomLabel.Name = "freedomLabel";
+            this.freedomLabel.Size = new System.Drawing.Size(48, 13);
+            this.freedomLabel.TabIndex = 5;
+            this.freedomLabel.Text = "Freedom";
+            // 
+            // dolceKInput
+            // 
+            this.dolceKInput.Location = new System.Drawing.Point(171, 134);
+            this.dolceKInput.Name = "dolceKInput";
+            this.dolceKInput.Size = new System.Drawing.Size(100, 20);
+            this.dolceKInput.TabIndex = 6;
+            // 
+            // lovebiteInput
+            // 
+            this.lovebiteInput.Location = new System.Drawing.Point(171, 191);
+            this.lovebiteInput.Name = "lovebiteInput";
+            this.lovebiteInput.Size = new System.Drawing.Size(100, 20);
+            this.lovebiteInput.TabIndex = 7;
+            // 
+            // freedomInput
+            // 
+            this.freedomInput.Location = new System.Drawing.Point(171, 246);
+            this.freedomInput.Name = "freedomInput";
+            this.freedomInput.Size = new System.Drawing.Size(100, 20);
+            this.freedomInput.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::Cash_Register_Summative.Properties.Resources.dolcek;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(64, 100);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Cash_Register_Summative.Properties.Resources.candyk;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(66, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 54);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = global::Cash_Register_Summative.Properties.Resources.lovebite;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox3.Location = new System.Drawing.Point(64, 156);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 55);
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImage = global::Cash_Register_Summative.Properties.Resources.freedom;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox4.Location = new System.Drawing.Point(63, 217);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(101, 52);
+            this.pictureBox4.TabIndex = 12;
+            this.pictureBox4.TabStop = false;
+            // 
+            // CheckoutButton
+            // 
+            this.CheckoutButton.Location = new System.Drawing.Point(162, 289);
+            this.CheckoutButton.Name = "CheckoutButton";
+            this.CheckoutButton.Size = new System.Drawing.Size(109, 23);
+            this.CheckoutButton.TabIndex = 13;
+            this.CheckoutButton.Text = "Checkout Now";
+            this.CheckoutButton.UseVisualStyleBackColor = true;
+            this.CheckoutButton.Click += new System.EventHandler(this.CheckoutButton_Click);
+            // 
+            // cashRegisterSummative
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Cash_Register_Summative.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(522, 337);
+            this.Controls.Add(this.CheckoutButton);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.freedomInput);
+            this.Controls.Add(this.lovebiteInput);
+            this.Controls.Add(this.dolceKInput);
+            this.Controls.Add(this.freedomLabel);
+            this.Controls.Add(this.lovebiteLabel);
+            this.Controls.Add(this.dolceKLabel);
+            this.Controls.Add(this.candyKLabel);
+            this.Controls.Add(this.candyKInput);
+            this.Controls.Add(this.lipkitTitle);
+            this.DoubleBuffered = true;
+            this.Name = "cashRegisterSummative";
+            this.Text = "Register Summative";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lipkitTitle;
+        private System.Windows.Forms.TextBox candyKInput;
+        private System.Windows.Forms.Label candyKLabel;
+        private System.Windows.Forms.Label dolceKLabel;
+        private System.Windows.Forms.Label lovebiteLabel;
+        private System.Windows.Forms.Label freedomLabel;
+        private System.Windows.Forms.TextBox dolceKInput;
+        private System.Windows.Forms.TextBox lovebiteInput;
+        private System.Windows.Forms.TextBox freedomInput;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button CheckoutButton;
+    }
+}
+
---
+++ Cash Register Summative/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cash_Register_Summative
+{
+    public partial class cashRegisterSummative : Form
+    {
+        public cashRegisterSummative()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckoutButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
